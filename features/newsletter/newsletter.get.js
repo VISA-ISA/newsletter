927f509f302a2278d4991769d92fd0eb76e56888
Kazerlelutin <b@bouteiller.contact>
2026-01-15 23:12:00 +0100

    console

--- features/newsletter/newsletter.get.js
+++ features/newsletter/newsletter.get.js
@@ -2,6 +2,7 @@ const Newsletter = require('./newsletter');
 
 module.exports = async (_, h) => {
 
+  console.log('newsletter.get');
   const newsletter = await Newsletter()
 
   return h.response(newsletter);
