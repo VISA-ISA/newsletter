7337fb56898b69e2229fa2b29f81798b550bf653
Kazerlelutin <b@bouteiller.contact>
2026-01-15 22:53:28 +0100

    test

--- features/cron/cron.js
+++ features/cron/cron.js
@@ -1,74 +1,94 @@
 const notif = require('../notif/notif');
 
-module.exports = [{
-  name: 'clean-sticky-posts',
-  time: '0 * * * *', // Toutes les heures
-  timezone: 'Europe/Paris',
-  request: {
-    method: 'POST',
-    url: '/clean-sticky-posts',
-    headers: {
-      'Authorization': `${process.env.CRON_TOKEN}`,
+module.exports = [
+  {
+    name: 'test',
+    time: '* 4 * * * *', // Toutes les 4 minutes
+    timezone: 'Europe/Paris',
+    request: {
+      method: 'GET',
+      url: '/',
+    },
+    onSuccess: (res) => {
+      console.log('✅ Test cron :', res.payload);
+    },
+    onError: (err) => {
+      notif(
+        `❌ Erreur lors du nettoyage des articles épinglés :
+          :warning: ${err}`
+      )
     },
   },
-  onError: (err) => {
-    notif(
-      `❌ Erreur lors du nettoyage des articles épinglés :
+
+  {
+    name: 'clean-sticky-posts',
+    time: '0 * * * *', // Toutes les heures
+    timezone: 'Europe/Paris',
+    request: {
+      method: 'POST',
+      url: '/clean-sticky-posts',
+      headers: {
+        'Authorization': `${process.env.CRON_TOKEN}`,
+      },
+    },
+    onError: (err) => {
+      notif(
+        `❌ Erreur lors du nettoyage des articles épinglés :
         :warning: ${err}`
-    )
-  },
-},
-{
-  name: 'newsletter-13h',
-  time: '0 13 * * *',
-  timezone: 'Europe/Paris',
-  request: {
-    method: 'POST',
-    url: '/',
-    headers: {
-      'Authorization': `${process.env.CRON_TOKEN}`,
+      )
     },
   },
-  onError: (err) => {
-    notif(
-      `❌ Erreur lors de l'envoi de la newsletter (13h) :
+  {
+    name: 'newsletter-13h',
+    time: '0 13 * * *',
+    timezone: 'Europe/Paris',
+    request: {
+      method: 'POST',
+      url: '/',
+      headers: {
+        'Authorization': `${process.env.CRON_TOKEN}`,
+      },
+    },
+    onError: (err) => {
+      notif(
+        `❌ Erreur lors de l'envoi de la newsletter (13h) :
         :warning: ${err}`
-    )
-  },
-},
-{
-  name: 'newsletter-15h',
-  time: '0 15 * * *',
-  timezone: 'Europe/Paris',
-  request: {
-    method: 'POST',
-    url: '/',
-    headers: {
-      'Authorization': `${process.env.CRON_TOKEN}`,
+      )
     },
   },
-  onError: (err) => {
-    notif(
-      `❌ Erreur lors de l'envoi de la newsletter (15h) :
+  {
+    name: 'newsletter-15h',
+    time: '0 15 * * *',
+    timezone: 'Europe/Paris',
+    request: {
+      method: 'POST',
+      url: '/',
+      headers: {
+        'Authorization': `${process.env.CRON_TOKEN}`,
+      },
+    },
+    onError: (err) => {
+      notif(
+        `❌ Erreur lors de l'envoi de la newsletter (15h) :
         :warning: ${err}`
-    )
-  },
-},
-{
-  name: 'newsletter-17h',
-  time: '0 17 * * *',
-  timezone: 'Europe/Paris',
-  request: {
-    method: 'POST',
-    url: '/',
-    headers: {
-      'Authorization': `${process.env.CRON_TOKEN}`,
+      )
     },
   },
-  onError: (err) => {
-    notif(
-      `❌ Erreur lors de l'envoi de la newsletter (17h) :
+  {
+    name: 'newsletter-17h',
+    time: '0 17 * * *',
+    timezone: 'Europe/Paris',
+    request: {
+      method: 'POST',
+      url: '/',
+      headers: {
+        'Authorization': `${process.env.CRON_TOKEN}`,
+      },
+    },
+    onError: (err) => {
+      notif(
+        `❌ Erreur lors de l'envoi de la newsletter (17h) :
         :warning: ${err}`
-    )
-  },
-}]
+      )
+    },
+  }]
